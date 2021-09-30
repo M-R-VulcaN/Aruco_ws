6c6db9bc10a602f58628d77af072e46958ef6f1b
Steven-Sorokin <stevensorok@gmail.com>
2021-09-30 16:24:42 +0300

    Added ability to choose the path of the directories used in fromLabelsToData.py via argv

--- src/aruco_tf/src/src/fromLablesToData.py
+++ src/aruco_tf/src/src/fromLablesToData.py
@@ -1,10 +1,27 @@
 import intervaltree
+import sys
 
 START_TIME_INDEX = 1
 END_TIME_INDEX = 2
 LABEL_INDEX = 3
-room_number = input("enter room number: ")
-CSV_LABELS_FILENAME = "room_" + str(room_number) + "_labels.csv"
+
+CSV_LABELS_FILENAME = ""
+OUTPUT_FILE_FILENAME = ""
+ARUCO_POSITION_FILENAME = ""
+
+CSV_LABELS_FILE_PATH_INDEX = 1
+ARUCO_POSITIONS_FILE_PATH_INDEX = 2
+OUTPUT_FILE_PATH_INDEX = 3
+
+if len(sys.argv) < 4:
+    room_number = input("enter room number: ")
+    CSV_LABELS_FILENAME = "room_" + str(room_number) + "_labels.csv"
+    OUTPUT_FILE_FILENAME = "room_" + str(room_number) + ".csv"
+    ARUCO_POSITION_FILENAME = "room_" + str(room_number) + "_aruco.csv"
+else:
+    CSV_LABELS_FILENAME = sys.argv[CSV_LABELS_FILE_PATH_INDEX]
+    CSV_LABELS_FILENAME = sys.argv[OUTPUT_FILE_PATH_INDEX]
+    CSV_LABELS_FILENAME = sys.argv[ARUCO_POSITIONS_FILE_PATH_INDEX]
 
 def get_label_intervaltree(csv_labels_filename = CSV_LABELS_FILENAME): 
     f = open(csv_labels_filename, "r")
@@ -29,8 +46,8 @@ def get_label_intervaltree(csv_labels_filename = CSV_LABELS_FILENAME):
 
 def main():
     
-    res_file = open("room_" + str(room_number) + ".csv", "w")
-    tofill = open("room_" + str(room_number) + "_aruco.csv", "r").read()       # aruco output
+    res_file = open(OUTPUT_FILE_FILENAME, "w")
+    tofill = open(ARUCO_POSITION_FILENAME, "r").read()       # aruco output
     data = get_label_intervaltree(CSV_LABELS_FILENAME)
 
     for line in tofill.split('\n')[1:-1]:
