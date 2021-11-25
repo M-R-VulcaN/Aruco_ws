adc818eb134e4f608f12b4e86207c1433db3855b
Steven-Sorokin <stevensorok@gmail.com>
2021-11-25 19:05:43 +0200

    Renamed a few elements and progressed a bit in the code

--- src/pcap_combined_with_subtitles/pcap_with_subtitles.py
+++ src/pcap_combined_with_subtitles/pcap_with_subtitles.py
@@ -8,6 +8,7 @@ PCAP_FILES_AMOUNT = 4
 #<paths>
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 RES_DIR = f'{ROOT_DIR}/res'
+OUTPUT_DIR = f'{ROOT_DIR}/output'
 
 #<argv>
 if len(sys.argv) == 1:
@@ -26,14 +27,40 @@ except ValueError:
 #</argv> 
 
 ROOM_DIR = f'{RES_DIR}/room_{CURR_ROOM_NUM}'
-SUBTITLE_FILE_PATH = f'{ROOM_DIR}/room_{CURR_ROOM_NUM}_images.csv'
+LABELS_FILE_PATH = f'{ROOM_DIR}/room_{CURR_ROOM_NUM}_images.csv'
 IMAGE_NAMES_FILE_PATH = f'{ROOM_DIR}/image_names.txt'
-PCAP_FILES_PATHS = [f'{ROOM_DIR}/dataset_room_{CURR_ROOM_NUM}_pcap_{pcap_num}.csv' for pcap_num in range(PCAP_FILES_AMOUNT)]
+OUTPUT_FILES_PATHS = [f'{OUTPUT_DIR}/dataset_room_{CURR_ROOM_NUM}_pcap_{pcap_num}.csv' for pcap_num in range(PCAP_FILES_AMOUNT)]
+
+DATASET_FILES_PATHS = [f'{ROOM_DIR}/dataset_room_{CURR_ROOM_NUM}_pcap_{pcap_num}.csv' for pcap_num in range(PCAP_FILES_AMOUNT)]
 #</paths>
 
+
+
+def get_frame_timestamps(image_names_list):
+
+
 def main():
-    subtitles_file = open(SUBTITLE_FILE_PATH,'r')
-    pcap_0_file = open(PCAP_FILES_PATHS[0],'r')
+    labels_file = open(LABELS_FILE_PATH,'r')
+    image_names_file = open(IMAGE_NAMES_FILE_PATH,'r')
+    dataset_0_file = open(DATASET_FILES_PATHS[0],'r')
+    output_0_file = open(OUTPUT_FILES_PATHS[0],'w')
+
+    labels_reader = csv.reader(labels_file)
+    dataset_0_reader = csv.reader(dataset_0_file)
+    output_0_writer = csv.reader(output_0_file)
+
+    label_index = 0
+    dataset_index = 0
+
+    while True:
+        curr_row_dataset = next(dataset_0_reader)
+        curr_row_label = next(dataset_0_reader)
+
+
+
+    labels_file.close()
+    dataset_0_file.close()
+    output_0_file.close()
 
 if __name__ == '__main__':
     main()
